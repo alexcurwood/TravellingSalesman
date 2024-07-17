6463872c491e49ccd24183c3bcd79050af9b22b7
alexcurwood <149108865+alexcurwood@users.noreply.github.com>
2024-07-17 12:47:12 +0100

    Initialise repository

---
+++ wxnq87/AlgAbasic.py
@@ -0,0 +1,283 @@
+import os
+import sys
+import time
+import random
+
+def read_file_into_string(input_file, ord_range):
+    the_file = open(input_file, 'r') 
+    current_char = the_file.read(1) 
+    file_string = ""
+    length = len(ord_range)
+    while current_char != "":
+        i = 0
+        while i < length:
+            if ord(current_char) >= ord_range[i][0] and ord(current_char) <= ord_range[i][1]:
+                file_string = file_string + current_char
+                i = length
+            else:
+                i = i + 1
+        current_char = the_file.read(1)
+    the_file.close()
+    return file_string
+
+def remove_all_spaces(the_string):
+    length = len(the_string)
+    new_string = ""
+    for i in range(length):
+        if the_string[i] != " ":
+            new_string = new_string + the_string[i]
+    return new_string
+
+def integerize(the_string):
+    length = len(the_string)
+    stripped_string = "0"
+    for i in range(0, length):
+        if ord(the_string[i]) >= 48 and ord(the_string[i]) <= 57:
+            stripped_string = stripped_string + the_string[i]
+    resulting_int = int(stripped_string)
+    return resulting_int
+
+def convert_to_list_of_int(the_string):
+    list_of_integers = []
+    location = 0
+    finished = False
+    while finished == False:
+        found_comma = the_string.find(',', location)
+        if found_comma == -1:
+            finished = True
+        else:
+            list_of_integers.append(integerize(the_string[location:found_comma]))
+            location = found_comma + 1
+            if the_string[location:location + 5] == "NOTE=":
+                finished = True
+    return list_of_integers
+
+def build_distance_matrix(num_cities, distances, city_format):
+    dist_matrix = []
+    i = 0
+    if city_format == "full":
+        for j in range(num_cities):
+            row = []
+            for k in range(0, num_cities):
+                row.append(distances[i])
+                i = i + 1
+            dist_matrix.append(row)
+    elif city_format == "upper_tri":
+        for j in range(0, num_cities):
+            row = []
+            for k in range(j):
+                row.append(0)
+            for k in range(num_cities - j):
+                row.append(distances[i])
+                i = i + 1
+            dist_matrix.append(row)
+    else:
+        for j in range(0, num_cities):
+            row = []
+            for k in range(j + 1):
+                row.append(0)
+            for k in range(0, num_cities - (j + 1)):
+                row.append(distances[i])
+                i = i + 1
+            dist_matrix.append(row)
+    if city_format == "upper_tri" or city_format == "strict_upper_tri":
+        for i in range(0, num_cities):
+            for j in range(0, num_cities):
+                if i > j:
+                    dist_matrix[i][j] = dist_matrix[j][i]
+    return dist_matrix
+
+def read_in_algorithm_codes_and_tariffs(alg_codes_file):
+    flag = "good"
+    code_dictionary = {}   
+    tariff_dictionary = {}  
+    if not os.path.exists(alg_codes_file):
+        flag = "not_exist"  
+        return code_dictionary, tariff_dictionary, flag
+    ord_range = [[32, 126]]
+    file_string = read_file_into_string(alg_codes_file, ord_range)  
+    location = 0
+    EOF = False
+    list_of_items = []  
+    while EOF == False: 
+        found_comma = file_string.find(",", location)
+        if found_comma == -1:
+            EOF = True
+            sandwich = file_string[location:]
+        else:
+            sandwich = file_string[location:found_comma]
+            location = found_comma + 1
+        list_of_items.append(sandwich)
+    third_length = int(len(list_of_items)/3)
+    for i in range(third_length):
+        code_dictionary[list_of_items[3 * i]] = list_of_items[3 * i + 1]
+        tariff_dictionary[list_of_items[3 * i]] = int(list_of_items[3 * i + 2])
+    return code_dictionary, tariff_dictionary, flag
+
+
+input_file = "AISearchfile180.txt"
+
+if len(sys.argv) > 1:
+    input_file = sys.argv[1]
+
+path_for_city_files = "../city-files"
+    
+if os.path.isfile(path_for_city_files + "/" + input_file):
+    ord_range = [[32, 126]]
+    file_string = read_file_into_string(path_for_city_files + "/" + input_file, ord_range)
+    file_string = remove_all_spaces(file_string)
+    print("I have found and read the input file " + input_file + ":")
+else:
+    print("*** error: The city file " + input_file + " does not exist in the city-file folder.")
+    sys.exit()
+
+location = file_string.find("SIZE=")
+if location == -1:
+    print("*** error: The city file " + input_file + " is incorrectly formatted.")
+    sys.exit()
+    
+comma = file_string.find(",", location)
+if comma == -1:
+    print("*** error: The city file " + input_file + " is incorrectly formatted.")
+    sys.exit()
+    
+num_cities_as_string = file_string[location + 5:comma]
+num_cities = integerize(num_cities_as_string)
+print("   the number of cities is stored in 'num_cities' and is " + str(num_cities))
+
+comma = comma + 1
+stripped_file_string = file_string[comma:]
+distances = convert_to_list_of_int(stripped_file_string)
+
+counted_distances = len(distances)
+if counted_distances == num_cities * num_cities:
+    city_format = "full"
+elif counted_distances == (num_cities * (num_cities + 1))/2:
+    city_format = "upper_tri"
+elif counted_distances == (num_cities * (num_cities - 1))/2:
+    city_format = "strict_upper_tri"
+else:
+    print("*** error: The city file " + input_file + " is incorrectly formatted.")
+    sys.exit()
+
+dist_matrix = build_distance_matrix(num_cities, distances, city_format)
+print("   the distance matrix 'dist_matrix' has been built.")
+
+
+path_for_alg_codes_and_tariffs = "../alg_codes_and_tariffs.txt"
+
+code_dictionary, tariff_dictionary, flag = read_in_algorithm_codes_and_tariffs(path_for_alg_codes_and_tariffs)
+
+if flag != "good":
+    print("*** error: The text file 'alg_codes_and_tariffs.txt' does not exist.")
+    sys.exit()
+
+print("The codes and tariffs have been read from 'alg_codes_and_tariffs.txt':")
+
+my_user_name = "wxnq87"
+
+my_first_name = "Alex"
+my_last_name = "Curwood"
+
+algorithm_code = "BG"
+
+
+if not algorithm_code in code_dictionary:
+    print("*** error: the algorithm code " + algorithm_code + " is illegal")
+    sys.exit()
+print("   your algorithm code is legal and is " + algorithm_code + " -" + code_dictionary[algorithm_code] + ".")
+
+
+added_note = ""
+
+def basic_greedy_search(distances, start_city):
+    # Initializes visited, path, current city, and the total distance
+    visited = set()
+    path = [start_city]
+    current = start_city
+    total_distance = 0
+    while len(visited) < num_cities:
+        # Adds the current city to visited
+        visited.add(current)
+        min_distance = float('inf')
+        next_node = None
+        # Iterates through all of the unvisited neighbours of the current city and selects the closest one
+        for neighbour in range(num_cities):
+            if neighbour not in visited and distances[current][neighbour] < min_distance:
+                min_distance = distances[current][neighbour]
+                next_node = neighbour
+        if next_node is None:
+            break
+        # Adds the nearest neighbour to the path and adds the distance to the total distance
+        path.append(next_node)
+        total_distance += min_distance
+        current = next_node
+        # Returns the path and the distance from the current node to the end node added to the total distance
+    return path, (total_distance + distances[start_city][current])
+
+
+# Iterates through all possible starting nodes and outputs the best tour found
+
+best_distance = float('inf')
+best_tour = []
+
+for i in range(len(dist_matrix)):
+    current_tour, current_distance = basic_greedy_search(dist_matrix, i)
+    if current_distance <= best_distance:
+        best_distance = current_distance
+        best_tour = current_tour
+
+tour = best_tour
+tour_length = best_distance
+
+flag = "good"
+length = len(tour)
+for i in range(0, length):
+    if isinstance(tour[i], int) == False:
+        flag = "bad"
+    else:
+        tour[i] = int(tour[i])
+if flag == "bad":
+    print("*** error: Your tour contains non-integer values.")
+    sys.exit()
+if isinstance(tour_length, int) == False:
+    print("*** error: The tour-length is a non-integer value.")
+    sys.exit()
+tour_length = int(tour_length)
+if len(tour) != num_cities:
+    print("*** error: The tour does not consist of " + str(num_cities) + " cities as there are, in fact, " + str(len(tour)) + ".")
+    sys.exit()
+flag = "good"
+for i in range(0, num_cities):
+    if not i in tour:
+        flag = "bad"
+if flag == "bad":
+    print("*** error: Your tour has illegal or repeated city names.")
+    sys.exit()
+check_tour_length = 0
+for i in range(0, num_cities - 1):
+    check_tour_length = check_tour_length + dist_matrix[tour[i]][tour[i + 1]]
+check_tour_length = check_tour_length + dist_matrix[tour[num_cities - 1]][tour[0]]
+if tour_length != check_tour_length:
+    flag = print("*** error: The length of your tour is not " + str(tour_length) + "; it is actually " + str(check_tour_length) + ".")
+    sys.exit()
+print("You, user " + my_user_name + ", have successfully built a tour of length " + str(tour_length) + "!")
+
+local_time = time.asctime(time.localtime(time.time()))
+output_file_time = local_time[4:7] + local_time[8:10] + local_time[11:13] + local_time[14:16] + local_time[17:19]
+output_file_time = output_file_time.replace(" ", "0")
+script_name = os.path.basename(sys.argv[0])
+if len(sys.argv) > 2:
+    output_file_time = sys.argv[2]
+output_file_name = script_name[0:len(script_name) - 3] + "_" + input_file[0:len(input_file) - 4] + "_" + output_file_time + ".txt"
+
+f = open(output_file_name,'w')
+f.write("USER = " + my_user_name + " (" + my_first_name + " " + my_last_name + "),\n")
+f.write("ALGORITHM CODE = " + algorithm_code + ", NAME OF CITY-FILE = " + input_file + ",\n")
+f.write("SIZE = " + str(num_cities) + ", TOUR LENGTH = " + str(tour_length) + ",\n")
+f.write(str(tour[0]))
+for i in range(1,num_cities):
+    f.write("," + str(tour[i]))
+f.write(",\nNOTE = " + added_note)
+f.close()
+print("I have successfully written your tour to the tour file:\n   " + output_file_name + ".")
